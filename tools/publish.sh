2473eba1c2d29a7e9255e139e1fb56050b0697ff
Glen Campbell <glen@broadpool.com>
2015-10-26 10:28:09 -0700

    updated templates

--- tools/publish.sh
+++ tools/publish.sh
@@ -8,12 +8,18 @@ echo Regenerating...
 hugo
 echo Publishing...
 cd public
+echo ==
+echo Uploading HTML files...
+echo ==
 aws s3 sync . s3://galliard.xyz \
     --exclude '*.*' \
     --include '*.html' \
     --cache-control 'max-age=600' \
     --content-type 'text/html; charset=utf-8' \
     --delete
+echo ==
+echo Uploading non-HTML files...
+echo ==
 aws s3 sync . s3://galliard.xyz \
     --exclude '*.html' \
     --cache-control 'max-age=600' \
