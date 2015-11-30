60f71ef2ba584dc53617782250edd5d57dbb9684
Glen Campbell <glen@broadpool.com>
2015-11-30 13:05:40 -0800

    tool for converting wok to hugo files

--- tools/wok2hugo.sh
+++ tools/wok2hugo.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-for file in *markdown; do
-  base=`basename $file .markdown`
-  mv $base.markdown $base.md
-done
+SOURCEDIR=~/Documents/src/glencampbell.co/content
+TARGETDIR=~/Documents/src/galliard.xyz/content/post/TEMP
 
-for file in *.md; do
-  perl -p -i -e 's/^/---\n/ if ($.==1);' $file
-done
-
-perl -p -i -e 's/^title:\s(.*)$/title: "\1"/;' *md
-perl -p -i -e 's/^datetime:\s(.*) (.*)$/date: "\1T\2-07:00"/' *md
-
-for file in *.md; do
-  perl -p -i -e 's/^/author: "Glen Campbell"\n/ if ($.==3);' $file
+for filename in `find $SOURCEDIR -type f`; do
+    echo "File: " $filename
+    targetfile=`basename $filename | sed -e 's/.markdown$/.md/'`
+    echo "  Target: " $targetfile
+    awk 'BEGIN { print "---\n"; } /datetime/ { sub("datetime","date"); sub("$","-07:00"); } /web 2.0/ { sub("web 2.0","web-20"); } { print; }' < $filename > $TARGETDIR/$targetfile
 done
